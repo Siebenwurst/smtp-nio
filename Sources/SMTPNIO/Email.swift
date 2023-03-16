11df86cb911a9751b4f75f40203e4a45f89e1573
Timo <38291523+lovetodream@users.noreply.github.com>
2023-03-16 10:41:25 +0100

    feat: make email and email address properties public

--- Sources/SMTPNIO/Email.swift
+++ Sources/SMTPNIO/Email.swift
@@ -1,29 +1,29 @@
 import struct Foundation.Date
 
 public struct EmailAddress {
-    var name: String?
-    var address: String
+    public var name: String?
+    public var address: String
 }
 
 public struct Email {
-    var sender: String
-    var recipients: [String]
-    var subject: String?
-    var body: Body
+    public var sender: String
+    public var recipients: [String]
+    public var subject: String?
+    public var body: Body
 
     public struct Body {
-        var receivedFrom: String?
-        var messageID: String?
-        var from: EmailAddress
-        var to: [EmailAddress]
-        var subject: String
-        var date: Date
-        var mimeVersion: String?
-        var xPriority: String?
-        var xMailer: String?
-        var contentType: String?
-        var contentTransferEncoding: String?
-        var content: String
+        public var receivedFrom: String?
+        public var messageID: String?
+        public var from: EmailAddress
+        public var to: [EmailAddress]
+        public var subject: String?
+        public var date: Date
+        public var mimeVersion: String?
+        public var xPriority: String?
+        public var xMailer: String?
+        public var contentType: String?
+        public var contentTransferEncoding: String?
+        public var content: String
 
         struct Pending {
             var receivedFrom: String?
@@ -40,7 +40,7 @@ public struct Email {
             var content: String?
 
             func toFinal() -> Body? {
-                guard let from, let to, let subject, let contentType, let date else { return nil }
+                guard let from, let to, let contentType, let date else { return nil }
                 return Body(
                     receivedFrom: receivedFrom,
                     messageID: messageID,
