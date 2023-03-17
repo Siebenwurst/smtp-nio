88ef22a60e4b84ff869f710b3cc99863a7d4d141
Timo <38291523+lovetodream@users.noreply.github.com>
2023-03-17 08:03:36 +0100

    feat: make network implementation public

--- Sources/SMTPNIO/NetworkImplementation.swift
+++ Sources/SMTPNIO/NetworkImplementation.swift
@@ -1,11 +1,4 @@
-//
-//  File.swift
-//  
-//
-//  Created by Timo Zacherl on 16.03.23.
-//
-
-enum NetworkImplementation {
+public enum NetworkImplementation {
     /// POSIX sockets and NIO.
     case posix
 
@@ -14,7 +7,7 @@ enum NetworkImplementation {
 
     /// Return the best implementation available for this platform, that is NIOTransportServices
     /// when it is available or POSIX and NIO otherwise.
-    static var best: NetworkImplementation {
+    public static var best: NetworkImplementation {
         if #available(macOS 10.14, iOS 12.0, tvOS 12.0, watchOS 6.0, *) {
             return .transportServices
         } else {
