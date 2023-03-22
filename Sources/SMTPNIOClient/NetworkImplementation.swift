fc85ce85a83165fa1f38bf1a9a128ad2c861853d
Timo <38291523+lovetodream@users.noreply.github.com>
2023-03-22 09:28:29 +0100

    fix: always use posix on linux

--- Sources/SMTPNIOClient/NetworkImplementation.swift
+++ Sources/SMTPNIOClient/NetworkImplementation.swift
@@ -8,10 +8,14 @@ public enum NetworkImplementation {
     /// Return the best implementation available for this platform, that is NIOTransportServices
     /// when it is available or POSIX and NIO otherwise.
     public static var best: NetworkImplementation {
+        #if os(Linux)
+        return .posix
+        #else
         if #available(macOS 10.14, iOS 12.0, tvOS 12.0, watchOS 6.0, *) {
             return .transportServices
         } else {
             return .posix
         }
+        #endif
     }
 }
